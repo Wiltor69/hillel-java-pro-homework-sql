d79d9ca4c8ae4b9f9045ecfe262921e62e6f10d3
Viacheslav <19wiltor69@gmail.com>
2023-02-04 15:49:46 +0200

    First commit

---
+++ init.sql
@@ -0,0 +1,25 @@
+-- create database hillelhomework
+-- show databases
+-- use hillelhomework;
+--  create table Homework(
+--  id int primary key auto_increment,
+--  name varchar (50),
+--  description varchar(150)
+--  );
+
+--  create table Lesson (
+--  id int primary key auto_increment,
+--  name varchar (50),
+--  updatedAt varchar(150),
+--  homework_id int, foreign key (homework_id)  references Homework(id)  
+--  );
+
+--  create table Schedule (
+--  id int primary key auto_increment,
+-- name varchar (50),
+-- updatedAt varchar(150),
+-- lessons int, 
+-- foreign key (lessons) references Lesson (id),
+-- foreign key (lessons)  references Homework(id)
+-- );
+show tables
