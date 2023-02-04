7123932ccd1ad18892941dcff9e7389f94c46d7e
Viacheslav <19wiltor69@gmail.com>
2023-02-04 17:22:27 +0200

    Fixed mistake

--- init.sql
+++ init.sql
@@ -1,25 +1,32 @@
--- create database hillelhomework
--- show databases
--- use hillelhomework;
---  create table Homework(
---  id int primary key auto_increment,
---  name varchar (50),
---  description varchar(150)
---  );
+create database hillelhomework
 
---  create table Lesson (
---  id int primary key auto_increment,
---  name varchar (50),
---  updatedAt varchar(150),
---  homework_id int, foreign key (homework_id)  references Homework(id)  
---  );
+use hillelhomework;
+create table Homework(
+ id int primary key auto_increment,
+name varchar (50),
+ description varchar(150)
+ );
+ 
+create table Lesson (
+id int primary key auto_increment,
+ name varchar (50),
+updatedAt varchar(150),
+homework_id int unique,  foreign key (homework_id) references Homework(id)  
+ );
 
---  create table Schedule (
---  id int primary key auto_increment,
--- name varchar (50),
--- updatedAt varchar(150),
--- lessons int, 
--- foreign key (lessons) references Lesson (id),
--- foreign key (lessons)  references Homework(id)
--- );
+create table Schedule (
+id int primary key auto_increment,
+name varchar (50),
+updatedAt varchar(150),
+lessons int
+);
+
+create table ScheduleLesson (
+id int primary key auto_increment,
+lessonId int,
+scheduleId int,
+foreign key (lessonId) references Lesson(id),
+foreign key (scheduleId) references Schedule(id)
+);
 show tables
+
